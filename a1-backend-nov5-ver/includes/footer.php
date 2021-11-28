33074d4133e6c75c3834279902db8f7f8aa0aba2
pepe27 <83622952+pepe27@users.noreply.github.com>
2021-11-27 21:25:48 -0500

    made js file

--- a1-backend-nov5-ver/includes/footer.php
+++ a1-backend-nov5-ver/includes/footer.php
@@ -5,5 +5,6 @@
 <footer>
     Hello! Please note we use cookies to provide a better site experience! <a href="#">Click to accept cookies</a>
 </footer>
+<script src="js/main.js"></script>
 </body>
 </html>
---
+++ a1-backend-nov5-ver/js/main.js
@@ -0,0 +1,2 @@
+//main.js
+console.log("connected main.js");
