271972418f65265cd9efcd410750e81e6b5940b5
pepe27 <83622952+pepe27@users.noreply.github.com>
2021-11-28 23:39:15 -0500

    dynamic site data working

--- a1-backend-nov5-ver/js/main.js
+++ a1-backend-nov5-ver/js/main.js
@@ -12,7 +12,44 @@ document.addEventListener('keydown', function(event) {
 
 let visitors = [
     {
+        month: "Month",
+        number: "Number of Site Visitors"
+    },{
         month: "Jan",
         number: 111
-    },{},{}
+    },{
+        month: "Feb",
+        number: 222
+    },{
+        month: "Mar",
+        number: 333
+    },{
+        month: "Apr",
+        number: 444
+    },{
+        month: "May",
+        number: 555
+    },{
+        month: "June",
+        number: 666
+    }
+    
 ];
+
+//1.reference
+let table = document.querySelectorAll("#siteVisitors")[0];
+
+for (let i=0; i<visitors.length; i++) {
+    let tableTr = document.createElement("tr");
+
+    let monthTh = document.createElement("th");
+    let numberTh = document.createElement("th");
+
+    monthTh.innerHTML = visitors[i].month;
+    numberTh.innerHTML = visitors[i].number;
+
+    table.appendChild(tableTr);
+    table.appendChild(monthTh);
+    table.appendChild(numberTh);
+
+}
